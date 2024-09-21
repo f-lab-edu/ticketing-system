acbc1d06bdeed17c3edfe2dd33f6e6382a8f0b2c
ahnsuyeon <plere0011@gmail.com>
2024-09-21 16:12:57 +0900

    요청 파라미터 validation exception advice 추가

--- admin/admin-concert/src/main/java/co/kr/ticketing/adminconcert/common/exception/ExceptionAdvice.java
+++ admin/admin-concert/src/main/java/co/kr/ticketing/adminconcert/common/exception/ExceptionAdvice.java
@@ -1,6 +1,8 @@
 package co.kr.ticketing.adminconcert.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +18,13 @@ public class ExceptionAdvice {
 			exception.getMessage());
 		log.warn("CustomRuntimeException 발생 : code: {}, message: {}", responseDto.getCode(), responseDto.getBody());
 		return new ResponseEntity<>(responseDto, exception.getStatus());
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDto<String>> customException(MethodArgumentNotValidException exception) {
+		HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+		ResponseDto<String> responseDto = new ResponseDto<>(badRequest.toString(), "요청 값이 잘 못 됐습니다");
+		log.warn("MethodArgumentNotValidException 발생 : message: {}", exception.getMessage());
+		return new ResponseEntity<>(responseDto, badRequest);
 	}
 }
