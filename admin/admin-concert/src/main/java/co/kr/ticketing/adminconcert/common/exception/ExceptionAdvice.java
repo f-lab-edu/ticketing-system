af6df1a5e57859ff0afaf7bd92a257003c3a904f
ahnsuyeon <plere0011@gmail.com>
2024-09-28 15:15:06 +0900

    코드리뷰 반영

--- admin/admin-concert/src/main/java/co/kr/ticketing/adminconcert/common/exception/ExceptionAdvice.java
+++ admin/admin-concert/src/main/java/co/kr/ticketing/adminconcert/common/exception/ExceptionAdvice.java
@@ -16,7 +16,7 @@ public class ExceptionAdvice {
 	public ResponseEntity<ResponseDto<String>> customException(CustomException exception) {
 		ResponseDto<String> responseDto = new ResponseDto<>(exception.getStatus().toString(),
 			exception.getMessage());
-		log.warn("CustomRuntimeException 발생 : code: {}, message: {}", responseDto.getCode(), responseDto.getBody());
+		log.error("CustomRuntimeException 발생 : code: {}, message: {}", responseDto.getCode(), responseDto.getBody());
 		return new ResponseEntity<>(responseDto, exception.getStatus());
 	}
 
@@ -24,7 +24,7 @@ public class ExceptionAdvice {
 	public ResponseEntity<ResponseDto<String>> customException(MethodArgumentNotValidException exception) {
 		HttpStatus badRequest = HttpStatus.BAD_REQUEST;
 		ResponseDto<String> responseDto = new ResponseDto<>(badRequest.toString(), "요청 값이 잘 못 됐습니다");
-		log.warn("MethodArgumentNotValidException 발생 : message: {}", exception.getMessage());
+		log.error("MethodArgumentNotValidException 발생 : message: {}", exception.getMessage());
 		return new ResponseEntity<>(responseDto, badRequest);
 	}
 }
