544fa568877cb9b9d73e01884a7c10cdaf51afff
ahnsuyeon <plere0011@gmail.com>
2024-11-01 16:20:44 +0900

    member-concert 모듈 추가

---
+++ member-concert/src/main/java/co/kr/ticketing/memberconcert/MemberConcertApplication.java
@@ -0,0 +1,13 @@
+package co.kr.ticketing.memberconcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberConcertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberConcertApplication.class, args);
+	}
+
+}
