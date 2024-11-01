d8e77cf14f5346dc4466e465901e0737f7281a2e
ahnsuyeon <plere0011@gmail.com>
2024-11-01 16:20:44 +0900

    member-concert 모듈 추가

---
+++ member-concert/src/test/java/co/kr/ticketing/memberconcert/MemberConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.ticketing.memberconcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberConcertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
